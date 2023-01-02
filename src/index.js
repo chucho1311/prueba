fcb65cf87e6e4bb1b7741e901f3d31d892eb7349
chucho1311 <carlosscristancho69@gmail.com>
2023-01-02 12:58:17 -0500

    prueba del back-end

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
-const v1WorkoutRouter = require("./v1/routes/workoutRoutes")
+const apicache = require("apicache");
+const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
+const {swaggerDocs: V1SwaggerDocs} = require('./v1/swagger');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const cache = apicache.middleware;
 
 app.use(express.json());
+app.use(cache("2 minutes"));
 app.use("/api/v1", v1WorkoutRouter);
 
 app.listen(PORT, () => {
-    console.log(`Serverlistening on port ${PORT}, succesfull`)
+    console.log(`Server listening on port ${PORT}, succesfull`);
+    V1SwaggerDocs(app, PORT);
 });
--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -4,10 +4,9 @@ const apicache = require('apicache');
 const workoutController = require("../../controllers/workoutController");
 const recordController = require("../../controllers/recordController");
 
-const cache = apicache.middleware;
 
 router 
-    .get("/", cache("2 minutes"), workoutController.getAllWorkouts)
+    .get("/", workoutController.getAllWorkouts)
     .get("/:workoutId", workoutController.getOneWorkout)
     .get("/:workoutId/records", recordController.getRecordForWorkout)
     .post("/", workoutController.createNewWorkout)
