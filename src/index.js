4c822657c93d8becbb3da100e5f956dc7e67d6ef
chucho1311 <carlosscristancho69@gmail.com>
2023-01-02 14:18:07 -0500

    cors save installed

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const apicache = require("apicache");
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 const {swaggerDocs: V1SwaggerDocs} = require('./v1/swagger');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001 ;
 const cache = apicache.middleware;
 
-header('Access-Control-Allow-Origin: *')
-
+app.use(cors())
 app.use(express.json());
 app.use(cache("2 minutes"));
 app.use("/api/v1", v1WorkoutRouter);
