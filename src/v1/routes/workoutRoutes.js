7a80ab0394aaaaf995534159d7c2f222e7c66d56
chucho1311 <carlosscristancho69@gmail.com>
2023-01-03 16:47:10 -0500

    comentarios en las rutas

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -3,16 +3,18 @@ const router = express.Router();
 const apicache = require('apicache');
 const workoutController = require("../../controllers/workoutController");
 const userController = require("../../controllers/recordController");
-const recordController = require("../../controllers/recordController");
 
 
 router 
+    //Rutas para la informarción de Pies
     .get("/pies", workoutController.getAllWorkouts)
-    .get("/users", userController.getAllUsers)
     .get("/:workoutId", workoutController.getOneWorkout)
-    .get("/:workoutId/records", recordController.getRecordForWorkout)
+    .get("/:workoutId/records", userController.getRecordForWorkout)
     .post("/", workoutController.createNewWorkout)
     .patch("/:workoutId", workoutController.updateOneWorkout)
     .delete("/:workoutId", workoutController.deleteOneWorkout)
+    //Rutas para la información de Usuarios
+    .get("/users", userController.getAllUsers)
+    
 
 module.exports = router;
